ae91d11c2ba74519d02ddcc630932ffb5f85517a
lyjeileen <lyjeileen@gmail.com>
2022-09-28 22:50:10 +0000

    add WASD movement

--- play.js
+++ play.js
@@ -2,5 +2,6 @@ const connect = require("./client");
 const setupInput = require("./input");
 
 console.log("Connecting ...");
-connect();
-setupInput();
+
+const conn = connect();
+setupInput(conn);
