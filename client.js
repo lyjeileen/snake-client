d743ecf5ab53223484d2032755880c8c9f23ab34
lyjeileen <lyjeileen@gmail.com>
2022-09-28 22:50:50 +0000

    add WASD movement

--- client.js
+++ client.js
@@ -4,17 +4,16 @@ const connect = function () {
     host: "165.227.47.243",
     port: 50541,
   });
+
   conn.on("data", (data) => {
     console.log(data);
   });
+
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: LYJ");
-    setTimeout(() => conn.write("Move: up"), 500);
-    setTimeout(() => conn.write("Move: left"), 1000);
-    setTimeout(() => conn.write("Move: down"), 1500);
-    setTimeout(() => conn.write("Move: right"), 2000);
   });
+
   conn.setEncoding("utf8");
 
   return conn;
--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -6,9 +9,22 @@ const setupInput = function () {
   stdin.on("data", handleUserInput);
   return stdin;
 };
+
 const handleUserInput = function (key) {
   if (key === "\u0003") {
     process.exit();
+  }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
   }
 };
 
